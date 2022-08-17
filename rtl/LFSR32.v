551e3b998fdc9d944245fb66876ca28b4f95c7ec
Michele Risino <michele.risino@edu.unito.it>
2022-08-17 21:59:02 +0200

    Miglioramento compattezza del codice

--- rtl/LFSR32.v
+++ rtl/LFSR32.v
@@ -1,5 +1,10 @@
 `timescale 1ns / 100ps
 
+//Nota: questo NON è un generatore di numeri casuali di per sè, in quanto è privo di una
+//randomness source. Agisce come un contatore che enumera i valori di 32 bit in ordine sparso.
+//Per generare numeri casuali si opera l'LFSR ad alta velocità (100 MHz) e si usa come randomness
+//source il tempo a cui l'utente preme il pulsante start.
+
 module LFSR32 (
     input wire clk,
     input wire rst,
@@ -17,33 +22,8 @@ module LFSR32 (
             rng_out <= 32'b11010100101001010110101010101101;
         else if (en) begin
             rng_out[0] <= feedback;
-            rng_out[1] <= rng_out[0];
-            rng_out[2] <= rng_out[1];
-            rng_out[3] <= rng_out[2];
-            rng_out[4] <= rng_out[3];
-            rng_out[5] <= rng_out[4];
-            rng_out[6] <= rng_out[5];
-            rng_out[7] <= rng_out[6];
-            rng_out[8] <= rng_out[7];
-            rng_out[9] <= rng_out[8];
-            rng_out[10] <= rng_out[9];
-            rng_out[11] <= rng_out[10];
-            rng_out[12] <= rng_out[11];
-            rng_out[13] <= rng_out[12];
-            rng_out[14] <= rng_out[13];
-            rng_out[15] <= rng_out[14];
-            rng_out[16] <= rng_out[15];
-            rng_out[17] <= rng_out[16];
-            rng_out[18] <= rng_out[17];
-            rng_out[19] <= rng_out[18];
-            rng_out[20] <= rng_out[19];
-            rng_out[21] <= rng_out[20];
-            rng_out[22] <= rng_out[21];
-            rng_out[23] <= rng_out[22];
-            rng_out[24] <= rng_out[23];
-            rng_out[25] <= rng_out[24];
-            rng_out[26] <= rng_out[25];
-            rng_out[27] <= rng_out[26];
+            for (integer i = 1; i < 28; i = i + 1)
+                rng_out[i] <= rng_out[i - 1];
             rng_out[28] <= rng_out[27] ^ feedback;
             rng_out[29] <= rng_out[28];
             rng_out[30] <= rng_out[29] ^ feedback;
