fb301899cc0ed10d42df5d59ce5ec7925b53acf2
Michele Risino <michele.risino@edu.unito.it>
2022-09-08 11:19:44 +0200

    Rallentamento debouncer

--- rtl/Debouncer.v
+++ rtl/Debouncer.v
@@ -4,9 +4,9 @@
 //per garantire una durata dell'impulso pari a un periodo di clock
 
 module Debouncer (
-   input  wire clk,            // assume 100 MHz clock frequency from on-board oscillator
-   input  wire button,         // glitching push-button input
-   output wire pulse           // clean single-pulse output
+   input wire clk,            // assume 100 MHz clock frequency from on-board oscillator
+   input wire button,         // glitching push-button input
+   output reg pulse           // clean single-pulse output
    ) ;
 
    ////////////////////////////////////////
@@ -15,21 +15,28 @@ module Debouncer (
 
    wire enable ;
 
-   TickCounter #(.MAX(1000001)) TickCounter_inst (.clk(clk), .tick(enable)) ;   // 0.5 kHz clock-enable
+   TickCounter #(.MAX(5000001)) TickCounter_inst (.clk(clk), .tick(enable)) ;   // 0.5 kHz clock-enable
 //   TickCounter #(.MAX(13)) TickCounter_inst (.clk(clk), .tick(enable)) ;   // 100 MHz clock-enable (for simulation purpose)
 
    ////////////////////////////////
    //   single-pulse generator   // tick length = period of TickCounter
    ////////////////////////////////
 
-   reg [2:0] q1 = 'b0 ;   // 3 FlipFlops
+   reg [1:0] q1 = 'b0 ;   // 2 FlipFlops
 
    always @(posedge clk) begin
-
+      
+      pulse <= 1'b0;
+      
       if(enable) begin
          q1[0] <= button ;
          q1[1] <= q1[0] ;
-         q1[2] <= q1[0] & (~q1[1]) ;
+         //q1[2] <= q1[0] & (~q1[1]) ;
+      end
+      
+      if (q1[0] & (~q1[1])) begin
+         q1[1] <= 1'b1;
+         pulse <= 1'b1;
       end
    end
    
@@ -37,7 +44,7 @@ module Debouncer (
    //   single-pulse generator   // tick length = period of clk
    ////////////////////////////////
 
-   reg [2:0] q2 = 'b0 ;   // 3 FlipFlops
+   /*reg [2:0] q2 = 'b0 ;   // 3 FlipFlops
 
    always @(posedge clk) begin
       q2[0] <= q1[2] ;
@@ -45,7 +52,7 @@ module Debouncer (
       q2[2] <= q2[0] & (~q2[1]) ;
    end
 
-   assign pulse = q2[2] ;
+   assign pulse = q2[2] ;*/
 
 endmodule
 
